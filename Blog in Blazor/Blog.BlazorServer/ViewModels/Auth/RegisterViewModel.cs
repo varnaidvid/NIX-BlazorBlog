5e25b7da95bbc7ddde11da406756d8acb340ea4c
Tordai Dengezik <sand@dsf.hu>
2025-04-02 16:24:40 +0200

    TASK 12 done

--- Blog in Blazor/Blog.BlazorServer/ViewModels/Auth/RegisterViewModel.cs
+++ Blog in Blazor/Blog.BlazorServer/ViewModels/Auth/RegisterViewModel.cs
@@ -4,15 +4,24 @@ namespace Blog.BlazorServer.ViewModels.Auth
 {
     public class RegisterViewModel
     {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 15 characters")]
+        [Display(Name = "Username")]
         public string Username { get; set; }
 
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "Confirm password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         public string ConfirmPassword { get; set; }
