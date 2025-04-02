21a353bcb2d46e40ab9cce30914391b08e6afc1f
varnaidvid <varnaidavid0522@gmail.com>
2025-04-02 16:24:03 +0200

    feat: task #10 comments gets refreshed on creation

--- Blog in Blazor/Blog.Business/Interfaces/ICommentService.cs
+++ Blog in Blazor/Blog.Business/Interfaces/ICommentService.cs
@@ -1,14 +1,16 @@
-﻿using Blog.Business.DTOs;
+using Blog.Business.DTOs;
+using System.Collections.Generic;
 
 namespace Blog.Business.Interfaces
 {
-    public interface ICommentService
-    {
-        Task CreateAsync(CommentDTO commentDTO);
+  public interface ICommentService
+  {
+    Task CreateAsync(CommentDTO commentDTO);
 
-        Task DeleteAsync(int id); 
+    Task DeleteAsync(int id);
 
-        Task<CommentDTO> GetByIdAsync(int id);
+    Task<CommentDTO> GetByIdAsync(int id);
 
-    }
+    Task<List<CommentDTO>> GetByPostIdAsync(int postId);
+  }
 }
--- Blog in Blazor/Blog.WebAPI/Controllers/CommentController.cs
+++ Blog in Blazor/Blog.WebAPI/Controllers/CommentController.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Claims;
+using System.Security.Claims;
 using Blog.Business.DTOs;
 using Blog.Business.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -6,52 +6,71 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.WebAPI.Controllers
 {
-    [Route("api/comment")]
-    [ApiController]
-    [Authorize]
-    public class CommentController : ControllerBase
+  [Route("api/comment")]
+  [ApiController]
+  [Authorize]
+  public class CommentController : ControllerBase
+  {
+    private readonly ICommentService _commentService;
+
+    public CommentController(ICommentService commentService)
     {
-        private readonly ICommentService _commentService;
-
-        public CommentController(ICommentService commentService)
-        {
-            _commentService = commentService;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateComment([FromBody] CommentDTO commentDTO)
-        {
-            if (commentDTO == null)
-            {
-                return BadRequest("CommentDTO cannot be null.");
-            }
-
-            var user = HttpContext.User;
-            var userName = user.FindFirst(ClaimTypes.Name)?.Value;
-
-            if (string.IsNullOrEmpty(userName))
-            {
-                return BadRequest("User name not found in claims.");
-            }
-
-            commentDTO.CreatedBy = userName;
-
-            await _commentService.CreateAsync(commentDTO);
-            return Created();
-        }
-
-        [HttpDelete("{id}")]
-        [Authorize]
-        public async Task<IActionResult> DeleteComment(int id)
-        {
-            var existingComment = await _commentService.GetByIdAsync(id);
-            if (existingComment == null)
-            {
-                return NotFound();
-            }
-
-            await _commentService.DeleteAsync(id);
-            return NoContent();
-        }
+      _commentService = commentService;
     }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateComment([FromBody] CommentDTO commentDTO)
+    {
+      if (commentDTO == null)
+      {
+        return BadRequest("CommentDTO cannot be null.");
+      }
+
+      var user = HttpContext.User;
+      var userName = user.FindFirst(ClaimTypes.Name)?.Value;
+
+      if (string.IsNullOrEmpty(userName))
+      {
+        return BadRequest("User name not found in claims.");
+      }
+
+      commentDTO.CreatedBy = userName;
+
+      await _commentService.CreateAsync(commentDTO);
+      return Created();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCommentById(int id)
+    {
+      var comment = await _commentService.GetByIdAsync(id);
+      if (comment == null)
+      {
+        return NotFound();
+      }
+      return Ok(comment);
+    }
+
+    [HttpGet("post/{postId}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetCommentsByPostId(int postId)
+    {
+      var comments = await _commentService.GetByPostIdAsync(postId);
+      return Ok(comments);
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize]
+    public async Task<IActionResult> DeleteComment(int id)
+    {
+      var existingComment = await _commentService.GetByIdAsync(id);
+      if (existingComment == null)
+      {
+        return NotFound();
+      }
+
+      await _commentService.DeleteAsync(id);
+      return NoContent();
+    }
+  }
 }
